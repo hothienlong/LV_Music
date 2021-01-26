2fe5ddfc9c399c4947084a452a411b51c442a8ec
oohoangoopro <long.ho460@hcmut.edu.vn>
2021-01-26 22:11:01 +0700

    play a music

--- app/src/main/java/com/example/lv_music/Activity/PlaySongActivity.java
+++ app/src/main/java/com/example/lv_music/Activity/PlaySongActivity.java
@@ -2,13 +2,18 @@ package com.example.lv_music.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 import com.example.lv_music.Adapter.PlaySongViewPagerAdapter;
@@ -16,8 +21,13 @@ import com.example.lv_music.Fragment.PlaySongFragment1;
 import com.example.lv_music.Fragment.PlaySongFragment2;
 import com.example.lv_music.Fragment.PlaySongFragment3;
 import com.example.lv_music.Model.Advertisement;
+import com.example.lv_music.Model.ApiResponse;
+import com.example.lv_music.Model.Song;
 import com.example.lv_music.Model.SongItem;
 import com.example.lv_music.R;
+import com.example.lv_music.ViewModel.LvMusicViewModel;
+
+import java.io.IOException;
 
 import me.relex.circleindicator.CircleIndicator;
 
@@ -28,7 +38,7 @@ public class PlaySongActivity extends AppCompatActivity {
     ImageView imgLikeSong, imgAddPlaylist, imgSuffle, imgBackward, imgPlaySong, imgForward, imgRepeat;
     CircleIndicator circleIndicator;
 
-    String mSongId;
+    LvMusicViewModel lvMusicViewModel;
 
 
     @Override
@@ -42,15 +52,35 @@ public class PlaySongActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        init();
+
 
     }
 
-    private void init() {
+    private void playMusic(String url) {
+
+        try {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioAttributes(
+                    new AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .build()
+            );
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepare(); // might take long! (for buffering, etc)
+            mediaPlayer.start();
+            Toast.makeText(this, "play", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void initLayoutFragment(String songId) {
         PlaySongViewPagerAdapter playSongViewPagerAdapter = new PlaySongViewPagerAdapter(getSupportFragmentManager());
-        playSongViewPagerAdapter.addFragment(new PlaySongFragment1(mSongId));
+        playSongViewPagerAdapter.addFragment(new PlaySongFragment1(songId));
         playSongViewPagerAdapter.addFragment(new PlaySongFragment2());
-        playSongViewPagerAdapter.addFragment(new PlaySongFragment3(mSongId));
+        playSongViewPagerAdapter.addFragment(new PlaySongFragment3(songId));
         viewPager.setAdapter(playSongViewPagerAdapter);
         // set circle indicator
         circleIndicator.setViewPager(viewPager);
@@ -80,19 +110,33 @@ public class PlaySongActivity extends AppCompatActivity {
     }
 
     private void catchIntent() {
+
         Intent intent = getIntent();
 
         if(intent != null){
+            lvMusicViewModel = ViewModelProviders.of(this).get(LvMusicViewModel.class);
+            String songId;
+
             if(intent.hasExtra("song")){
                 SongItem songItem = (SongItem) intent.getSerializableExtra("song");
 //              Toast.makeText(this, songItem.getName(), Toast.LENGTH_SHORT).show();
-                mSongId = songItem.getId();
+                songId = songItem.getId();
+                initLayoutFragment(songId);
+
+                lvMusicViewModel.getResponseSong().observe(this, new Observer<ApiResponse<Song>>() {
+                    @Override
+                    public void onChanged(ApiResponse<Song> songApiResponse) {
+                        playMusic(songApiResponse.getData().getSong_link());
+                    }
+                });
+
+                lvMusicViewModel.fetchSong(Integer.parseInt(songId));
             }
             else if(intent.hasExtra("advertisement")){
                 Advertisement advertisement = (Advertisement) intent.getSerializableExtra("advertisement");
 //                Log.d("BBB", advertisement.getSongId());
 //                Toast.makeText(this, advertisement.toString(), Toast.LENGTH_SHORT).show();
-                mSongId = advertisement.getSongId();
+                songId = advertisement.getSongId();
             }
         }
     }
